9f59591862a2fa5d5919c0d6d9e8c29977c9c23d
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-26 12:17:23 -0400

    one rspec away from finishing. Worked on with Sean Donohue and Johnnie Gonzalez.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,75 @@
-# your code goes here
+
+require 'pry'
+
+class Person
+    attr_reader :name  
+    attr_accessor :bank_account, :happiness, :hygiene  
+
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+        
+        
+    end
+    
+
+
+    def happiness
+        if @happiness > 10 
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+        @happiness
+    end
+
+    def hygiene
+        if @hygiene > 10 
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+        @hygiene
+    end
+
+    def happy?
+        self.happiness > 7
+                
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        return "all about the benjamins"
+    end
+    
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3 
+        friend.happiness += 3 
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    
+    end
+
+        
+
+
+end
+
+
